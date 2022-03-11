46b90432246a7ef9f6969b5faee0960ee4b50524
ashleygiannetti <ashley_giannetti@brown.edu>
2022-03-11 10:24:44 -0500

    if_statement
    if the sum is one it will display 1

--- the_sum.m
+++ the_sum.m
@@ -1,4 +1,7 @@
 function the_sum( num1, num2 )
 % ARITHMETIC: Takes two numbers, num1 and num2 and adds them 
 the_sum = num1 + num2; % add the numbers and call it the_sum 
+if the_sum == 1
+    dis('one')
+end
 end
